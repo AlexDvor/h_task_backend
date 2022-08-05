2e4596c39125584300599c0ba9bc7d22157edea9
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-08-05 19:14:12 +0200

    add

--- controllers/users/getCurrentUser.js
+++ controllers/users/getCurrentUser.js
@@ -1,17 +1,21 @@
 const { User } = require("../../models");
 
-const getCurrentUser = async (req, res) => {
+const getCurrentUser = async (req, res, next) => {
   const { _id } = req.user;
-  const result = await User.findOne({ _id });
 
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      name: result.name,
-      email: result.email,
-    },
-  });
+  try {
+    const result = await User.findOne({ _id });
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        name: result.name,
+        email: result.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getCurrentUser;
