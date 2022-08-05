871eda476d657d92361eb6d5c358c5054fbcd471
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-08-05 19:30:30 +0200

    add log out fun

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,6 +3,7 @@ const { User } = require("../../models");
 const logout = async (req, res, next) => {
   try {
     const { _id } = req.user;
+    console.log("🚀 - _id", _id);
     await User.findByIdAndUpdate(_id, { token: null });
     res.status(204).json();
   } catch (error) {
--- controllers/users/getCurrentUser.js
+++ controllers/users/getCurrentUser.js
@@ -2,7 +2,6 @@ const { User } = require("../../models");
 
 const getCurrentUser = async (req, res, next) => {
   const { _id } = req.user;
-
   try {
     const result = await User.findOne({ _id });
     res.json({
